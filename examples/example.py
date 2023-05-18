e5e97a7aa8864040218a4d913790b2a5f4e527fb
frostedoyster <bigi.f@libero.it>
2023-05-18 08:44:14 +0200

    Broken dgesv

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,12 @@
 import torch
+torch.set_default_dtype(torch.float64)
 from torch_mkl64 import mkl64
 
-A = torch.rand(2, 2)
-print(A)
+A = torch.rand(5, 5)
 d, O = mkl64.eigh64(A)
+assert torch.allclose(A, O @ torch.diag(d) @ O.T)
 
-print(d)
-print(O)
+A = torch.rand(5, 5)
+b = torch.rand(5)
+x = mkl64.dgesv(A, b)
+assert torch.allclose(b, A @ x)
--- torch_mkl64/mkl64.cc
+++ torch_mkl64/mkl64.cc
@@ -3,9 +3,9 @@
 #include <stdexcept>
 #include "mkl.h"
 
-std::vector<torch::Tensor> eigh64(torch::Tensor A) {
 
-    // A few checks:
+std::vector<torch::Tensor> dsyev(torch::Tensor A) {
+
     if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
     if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
     if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
@@ -15,11 +15,34 @@ std::vector<torch::Tensor> eigh64(torch::Tensor A) {
     torch::Tensor O = A.detach().clone();
     torch::Tensor d = torch::empty({A.sizes()[0]}, torch::TensorOptions().dtype(torch::kFloat64));
 
-    LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', n, O.data_ptr<double>(), n, d.data_ptr<double>());
+    int info = LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', n, O.data_ptr<double>(), n, d.data_ptr<double>());
+    if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
 
     return std::vector<torch::Tensor>{d, O};
 }
 
+
+torch::Tensor dgesv(torch::Tensor A, torch::Tensor b) {
+
+    if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
+    if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
+    if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
+    if (!A.is_contiguous()) throw std::runtime_error("not contiguous");
+    if (!b.is_contiguous()) throw std::runtime_error("not contiguous");
+    if (A.sizes()[0] != b.sizes()[0]) throw std::runtime_error("size inconsistency");
+
+    std::size_t n = A.sizes()[0];
+    torch::Tensor x = b.detach().clone();
+    std::vector<std::size_t> ipiv(n);
+
+    int info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, A.data_ptr<double>(), n, ipiv, x.data_ptr<double>(), 1);
+    if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
+
+    return x;
+}
+
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("eigh64", &eigh64, "torch.linalg.eigh64 for large matrices");
+    m.def("dsyev", &dsyev, "dsyev: diagonalization (symmetric)");
+    m.def("dgesv", &dgesv, "dgesv: linear solve (general)");
 }
