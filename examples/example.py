dcf17950bac14870c880638b45701ed1a8ba90f0
frostedoyster <bigi.f@libero.it>
2023-05-18 02:23:53 +0200

    Add dsysv

--- examples/example.py
+++ examples/example.py
@@ -3,12 +3,17 @@ torch.set_default_dtype(torch.float64)
 from torch_mkl64 import mkl64
 
 A = torch.rand(5, 5)
+A = A + A.T
 d, O = mkl64.dsyev(A)
-print(A)
-print(O @ torch.diag(d) @ O.T)
 assert torch.allclose(A, O @ torch.diag(d) @ O.T)
 
 A = torch.rand(5, 5)
 b = torch.rand(5)
 x = mkl64.dgesv(A, b)
+assert torch.allclose(b, A @ x)
+
+A = torch.rand(5, 5)
+A = A + A.T
+b = torch.rand(5)
+x = mkl64.dsysv(A, b)
 assert torch.allclose(b, A @ x)
--- torch_mkl64/mkl64.cc
+++ torch_mkl64/mkl64.cc
@@ -35,14 +35,37 @@ namespace mkl64 {
         if (A.sizes()[0] != b.sizes()[0]) throw std::runtime_error("size inconsistency");
 
         long long n = A.sizes()[0];
+        torch::Tensor A_copy = A.detach().clone();
         torch::Tensor x = b.detach().clone();
         std::vector<long long> ipiv(n);
 
-        int info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, A.data_ptr<double>(), n, ipiv.data(), x.data_ptr<double>(), 1);
+        int info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, A_copy.data_ptr<double>(), n, ipiv.data(), x.data_ptr<double>(), 1);
         if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
 
         return x;
     }
+
+
+    torch::Tensor dsysv(torch::Tensor A, torch::Tensor b) {
+
+        if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
+        if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
+        if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
+        if (!A.is_contiguous()) throw std::runtime_error("not contiguous");
+        if (!b.is_contiguous()) throw std::runtime_error("not contiguous");
+        if (A.sizes()[0] != b.sizes()[0]) throw std::runtime_error("size inconsistency");
+
+        long long n = A.sizes()[0];
+        torch::Tensor A_copy = A.detach().clone();
+        torch::Tensor x = b.detach().clone();
+        std::vector<long long> ipiv(n);
+
+        int info = LAPACKE_dsysv(LAPACK_ROW_MAJOR, 'U', n, 1, A_copy.data_ptr<double>(), n, ipiv.data(), x.data_ptr<double>(), 1);
+        if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
+
+        return x;
+    }
+
 
 }
 
@@ -50,4 +73,5 @@ namespace mkl64 {
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("dsyev", &mkl64::dsyev, "dsyev: diagonalization (symmetric)");
     m.def("dgesv", &mkl64::dgesv, "dgesv: linear solve (general)");
+    m.def("dsysv", &mkl64::dsysv, "dgesv: linear solve (symmetric)");
 }
