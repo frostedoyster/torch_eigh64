7798f6f6fdc41d13e172e9fef973d646b1859f67
frostedoyster <bigi.f@libero.it>
2023-05-18 09:12:33 +0200

    Use namespace

--- examples/example.py
+++ examples/example.py
@@ -3,7 +3,9 @@ torch.set_default_dtype(torch.float64)
 from torch_mkl64 import mkl64
 
 A = torch.rand(5, 5)
-d, O = mkl64.eigh64(A)
+d, O = mkl64.dsyev(A)
+print(A)
+print(O @ torch.diag(d) @ O.T)
 assert torch.allclose(A, O @ torch.diag(d) @ O.T)
 
 A = torch.rand(5, 5)
--- torch_mkl64/mkl64.cc
+++ torch_mkl64/mkl64.cc
@@ -4,45 +4,50 @@
 #include "mkl.h"
 
 
-std::vector<torch::Tensor> dsyev(torch::Tensor A) {
-
-    if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
-    if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
-    if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
-    if (!A.is_contiguous()) throw std::runtime_error("not contiguous");
-
-    long long n = A.sizes()[0];
-    torch::Tensor O = A.detach().clone();
-    torch::Tensor d = torch::empty({A.sizes()[0]}, torch::TensorOptions().dtype(torch::kFloat64));
-
-    int info = LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', n, O.data_ptr<double>(), n, d.data_ptr<double>());
-    if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
-
-    return std::vector<torch::Tensor>{d, O};
-}
+namespace mkl64 {
 
 
-torch::Tensor dgesv(torch::Tensor A, torch::Tensor b) {
+    std::vector<torch::Tensor> dsyev(torch::Tensor A) {
 
-    if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
-    if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
-    if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
-    if (!A.is_contiguous()) throw std::runtime_error("not contiguous");
-    if (!b.is_contiguous()) throw std::runtime_error("not contiguous");
-    if (A.sizes()[0] != b.sizes()[0]) throw std::runtime_error("size inconsistency");
+        if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
+        if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
+        if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
+        if (!A.is_contiguous()) throw std::runtime_error("not contiguous");
 
-    long long n = A.sizes()[0];
-    torch::Tensor x = b.detach().clone();
-    std::vector<long long> ipiv(n);
+        long long n = A.sizes()[0];
+        torch::Tensor O = A.detach().clone();
+        torch::Tensor d = torch::empty({A.sizes()[0]}, torch::TensorOptions().dtype(torch::kFloat64));
 
-    int info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, A.data_ptr<double>(), n, ipiv.data(), x.data_ptr<double>(), 1);
-    if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
+        int info = LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', n, O.data_ptr<double>(), n, d.data_ptr<double>());
+        if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
 
-    return x;
+        return std::vector<torch::Tensor>{d, O};
+    }
+
+
+    torch::Tensor dgesv(torch::Tensor A, torch::Tensor b) {
+
+        if (A.sizes().size() != 2) throw std::runtime_error("not two-dimensional");
+        if (A.sizes()[0] != A.sizes()[1]) throw std::runtime_error("not square");
+        if (A.dtype() != torch::kFloat64) throw std::runtime_error("not float64");
+        if (!A.is_contiguous()) throw std::runtime_error("not contiguous");
+        if (!b.is_contiguous()) throw std::runtime_error("not contiguous");
+        if (A.sizes()[0] != b.sizes()[0]) throw std::runtime_error("size inconsistency");
+
+        long long n = A.sizes()[0];
+        torch::Tensor x = b.detach().clone();
+        std::vector<long long> ipiv(n);
+
+        int info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, A.data_ptr<double>(), n, ipiv.data(), x.data_ptr<double>(), 1);
+        if (info != 0) std::cout << "Something went wrong: info code " << info << std::endl;
+
+        return x;
+    }
+
 }
 
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("dsyev", &dsyev, "dsyev: diagonalization (symmetric)");
-    m.def("dgesv", &dgesv, "dgesv: linear solve (general)");
+    m.def("dsyev", &mkl64::dsyev, "dsyev: diagonalization (symmetric)");
+    m.def("dgesv", &mkl64::dgesv, "dgesv: linear solve (general)");
 }
